f9fee2c2b94cf4c6884d9676150345888b6e5407
asanans <111622571+asanans@users.noreply.github.com>
2024-03-08 21:14:25 -0500

    Add New Square generator : prompts user for number of squares per side and generates a grid of squares

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 const containerEl = document.querySelector(".container")
 
-for (let i = 0; i < 17; i++) {
-    const rowEl = document.createElement("div")
-    rowEl.classList.add("row")
+let numberSquaresPerSide = 16
 
-for (let i = 0; i < 17; i++) {
-    const squareEl = document.createElement("div")
-    squareEl.classList.add("square")
-    rowEl.appendChild(squareEl)
-}
+createSquare()
 
-containerEl.appendChild(rowEl)
-}
+const newSquareBtn = document.querySelector(".new-square")
+newSquareBtn.addEventListener("click", selectSquarePerSide)
 
-const allSquares = document.querySelectorAll(".square")
-console.log(allSquares)
+function createSquare() {
+    containerEl.innerHTML = ""
 
-for (square of allSquares) {
-    square.addEventListener("mouseenter", changeColor)
+    for (let i = 0; i < numberSquaresPerSide; i++) {
+        const rowEl = document.createElement("div")
+        rowEl.classList.add("row")
+    
+        for (let i = 0; i < numberSquaresPerSide; i++) {
+            const squareEl = document.createElement("div")
+            squareEl.classList.add("square")
+            rowEl.appendChild(squareEl)
+        }
+    
+         containerEl.appendChild(rowEl)
+    }
+
+    const allSquares = document.querySelectorAll(".square")
+
+    for (square of allSquares) {
+        square.addEventListener("mouseenter", changeColor)
+    }
+
 }
 
 function changeColor() {
     this.classList.add("hover-effect")
     this.removeEventListener("mouseenter", changeColor)
+}
+
+function selectSquarePerSide() {
+    let inputSquaresPerSide = prompt("Please enter number of squares per side (max 100):")
+    numberSquaresPerSide = parseInt(inputSquaresPerSide)
+
+    if (!numberSquaresPerSide || numberSquaresPerSide > 100) {
+        alert("Invalid input. Please pick a number between 1-100!")
+        return
+    }
+    
+    createSquare()
 }
